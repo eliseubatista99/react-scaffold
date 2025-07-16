6a5c1d2d203f78de4e6ad4d0db643c21ee4e85f5
Eliseu Batista <eliseubatista99@outlook.com>
2025-07-16 17:58:35 +0100

    Fix packages dist

--- src/packages/firebase/vite.config.ts
+++ src/packages/firebase/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({ insertTypesEntry: true }), // Gera ficheiros .d.ts
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "MyComponentLibrary",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"], // evita que estas libs sejam incluídas no bundle
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+    outDir: "dist",
+  },
+});
--- src/packages/pdf/src/hooks/usePdfGenerator/stories/viteHomepage.tsx
+++ src/packages/pdf/src/hooks/usePdfGenerator/stories/viteHomepage.tsx
@@ -1,18 +1,10 @@
 import { useState } from "react";
-// import viteLogo from "/vite.svg";
-
-import reactLogo from "../../../../../../assets/react.svg";
 
 export const ViteHomepage = () => {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
