21eb39caf630266cb2b7f4fe5731c7489a16e666
Eliseu Batista <eliseubatista99@outlook.com>
2025-11-05 10:18:37 +0000

    + Add [CV]: Added curriculumVitae component do CV package

--- src/packages/cv/src/templates/baseTemplate.tsx
+++ src/packages/cv/src/templates/baseTemplate.tsx
@@ -2,13 +2,14 @@ import { Document } from "@react-pdf/renderer";
 import React from "react";
 
 type BaseCVTemplateProps = {
+  title?: string;
   children?: React.ReactNode;
 };
 
-export const BaseCVTemplate = ({ children }: BaseCVTemplateProps) => {
+export const BaseCVTemplate = ({ children, title }: BaseCVTemplateProps) => {
   return (
     <Document
-      title="CV"
+      title={title || "CV"}
       author="eliseubatista99"
       style={{ width: "100%", height: "100%", position: "relative", flex: 1 }}
     >
