7d1c767175cef9adffc4bf80234c95bba2375dac
Eliseu Batista <44680425+eliseubatista99@users.noreply.github.com>
2025-11-05 10:21:00 +0000

    + Add [CV]: Added curriculumVitae component do CV package

--- src/packages/cv/src/types/template.ts
+++ src/packages/cv/src/types/template.ts
@@ -2,6 +2,7 @@ import { Language } from "@eliseubatista99/react-scaffold-core";
 import { CvData } from "./cv";
 
 export interface CvTemplateProps {
+  title?: string;
   language?: Language;
   theme?: CvTheme;
   data: CvData;
