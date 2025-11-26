0dbcfd01abf6508fd022dd4b5b150561c2937ed2
Eliseu Batista <eliseubatista99@outlook.com>
2025-11-26 12:28:40 +0000

    time helper

--- src/packages/core/src/helpers/timeHelper/timeHelper.ts
+++ src/packages/core/src/helpers/timeHelper/timeHelper.ts
@@ -57,6 +57,14 @@ export class TimeHelper {
     return false;
   };
 
+  static isSameDate = (d1: Date, d2: Date): boolean => {
+    return (
+      d1.getFullYear() === d2.getFullYear() &&
+      d1.getMonth() === d2.getMonth() &&
+      d1.getDate() === d2.getDate()
+    );
+  };
+
   static getDateInUTC = (date: string | null | undefined) => {
     try {
       const utcDate = dayjs.utc(date);
