d2e93756d3fcdb9c7136d55eba4425d55d9f0ee2
Eliseu Batista <eliseubatista99@outlook.com>
2026-07-02 15:52:33 +0100

    fix storage input

--- src/packages/firebase/src/helpers/firebaseStorage/firebaseStorageHelper.ts
+++ src/packages/firebase/src/helpers/firebaseStorage/firebaseStorageHelper.ts
@@ -1,33 +1,35 @@
 import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 
+type UploadFileInput = {
+  file: File;
+  path?: string;
+  fileName?: string;
+};
+
 export class FirebaseStorageHelper {
-  static uploadImage = async (
-    file: File,
-    path = "images",
-    fileName?: string,
-  ): Promise<string> => {
-    if (!file.type.startsWith("image/")) {
+  static uploadImage = async (input: UploadFileInput): Promise<string> => {
+    if (!input.file.type.startsWith("image/")) {
       throw new Error("File must be an image");
     }
 
-    return await this.uploadFile(file, path, fileName);
+    input.path = input.path ?? "images";
+
+    return await this.uploadFile(input);
   };
 
-  static uploadFile = async (
-    file: File,
-    path = "files",
-    fileName?: string,
-  ): Promise<string> => {
+  static uploadFile = async (input: UploadFileInput): Promise<string> => {
     const storage = getStorage();
+    const fileName = input.fileName ?? input.file.name;
+    const path = input.path ?? "files";
 
-    const safeName = (fileName ?? file.name)
+    const safeName = fileName
       .normalize("NFD")
       .replace(/[\u0300-\u036f]/g, "")
       .replace(/\s+/g, "-");
 
     const fileRef = ref(storage, `${path}/${crypto.randomUUID()}-${safeName}`);
 
-    await uploadBytes(fileRef, file);
+    await uploadBytes(fileRef, input.file);
 
     const url = await getDownloadURL(fileRef);
 
