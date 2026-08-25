fcf1e43878fe9302364a2e128d34b11459771846
Eliseu Batista <eliseubatista99@outlook.com>
2026-08-25 07:43:48 +0100

    useDidMountWithFirebaseAuth

---
+++ src/packages/firebase/src/hooks/useDidMountWithFirebaseAuth/useDidMountWithFirebaseAuth.ts
@@ -0,0 +1,34 @@
+import { getAuth, User } from "firebase/auth";
+import React from "react";
+import { FirebaseAppHelper } from "../../helpers";
+
+export const useDidMountWithFirebaseAuth = (action: () => void) => {
+  React.useEffect(() => {
+    if (!FirebaseAppHelper.isAppInitialized()) {
+      throw new Error(
+        "useDidMountWithFirebaseAuth > Firebase app is not initialized.",
+      );
+    }
+
+    const firebaseApp = FirebaseAppHelper.getApp();
+    const auth = getAuth(firebaseApp!);
+
+    if (!auth) {
+      throw new Error(
+        "useDidMountWithFirebaseAuth > Firebase Auth is not initialized",
+      );
+    }
+
+    const unsubscribe = auth.onAuthStateChanged((user: User | null) => {
+      console.log("useDidMountWithFirebaseAuth > AuthStateChanged > ", {
+        user,
+      });
+
+      if (user) {
+        action();
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+};
